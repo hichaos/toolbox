6d0ec91bd7077830f1e077ba60f1d33cf5755fc4
chaos <hichaos@126.com>
2018-08-22 10:58:25 -0100

    add ByteKit

---
+++ src/main/java/com/chaos/ByteKit.java
@@ -0,0 +1,15 @@
+package com.chaos;
+
+/**
+ * Created by chaos on 2018/8/22.
+ */
+public interface ByteKit {
+
+    static String readableCount(long bytes, boolean si) {
+        int unit = si ? 1000 : 1024;
+        if (bytes < unit) return bytes + " B";
+        int exp = (int) (Math.log(bytes) / Math.log(unit));
+        String pre = (si ? "kMGTPE" : "KMGTPE").charAt(exp-1) + (si ? "" : "i");
+        return String.format("%.1f %sB", bytes / Math.pow(unit, exp), pre);
+    }
+}
